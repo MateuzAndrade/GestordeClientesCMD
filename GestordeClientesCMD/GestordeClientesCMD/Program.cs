fc790068bac4bbd3da7ce6d490d6edd7f64391a9
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-04-07 07:40:11 -0300

    ajustes

--- GestordeClientesCMD/GestordeClientesCMD/Program.cs
+++ GestordeClientesCMD/GestordeClientesCMD/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 
 namespace GestordeClientesCMD
@@ -24,6 +25,7 @@ namespace GestordeClientesCMD
 
         static void Main(string[] args)
         {
+            Carrregar();
             Console.ForegroundColor = ConsoleColor.Blue;
             bool escolheuSair = false;
 
@@ -49,6 +51,7 @@ namespace GestordeClientesCMD
                         Adicionar();
                         break;
                     case Menu.Remover:
+                        Remover();
                         break;
                     case Menu.Sair:
                         escolheuSair = true;
@@ -69,14 +72,14 @@ namespace GestordeClientesCMD
             Console.WriteLine("Lista de Clientes");
             Console.WriteLine("====================================\n");
 
-            int i = 1;
+            int id = 0;
             foreach (Cliente cliente in clientes)
             {
-                Console.WriteLine($"Cliente ID: {i}");
+                Console.WriteLine($"Cliente ID: {id}");
                 Console.WriteLine($"Nome: {cliente.Nome}");
                 Console.WriteLine($"E-mail: {cliente.email}");
                 Console.WriteLine($"CPF: {cliente.cpf}\n");
-                i++;
+                id++;
             }
             Console.WriteLine("Aperte enter para sair.");
             Console.ReadLine();
@@ -96,6 +99,7 @@ namespace GestordeClientesCMD
             cliente.cpf = Console.ReadLine();
 
             clientes.Add(cliente);
+            Salvar();
 
             Console.WriteLine("Cadastro Concluído, Aperte enter para sair");
             Console.ReadLine();
@@ -103,7 +107,61 @@ namespace GestordeClientesCMD
 
         static void Salvar()
         {
-            FileStream stream = new FileStream()
+            FileStream stream = new FileStream("clients.dat", FileMode.OpenOrCreate);
+            BinaryFormatter encoder = new BinaryFormatter();
+
+            encoder.Serialize(stream, clientes);
+
+            stream.Close();
+
         }
+
+        static void Carrregar()
+        {
+            FileStream stream = new FileStream("clients.dat", FileMode.OpenOrCreate);
+
+            try
+            {
+                BinaryFormatter encoder = new BinaryFormatter();
+
+                clientes = (List<Cliente>)encoder.Deserialize(stream);
+
+                if (clientes == null)
+                {
+                    clientes = new List<Cliente>();
+
+                }
+
+            }
+            catch (Exception)
+            {
+                clientes = new List<Cliente>();
+            }
+
+            stream.Close();
+
+        }
+
+        static void Remover()
+        {
+            Listagem();
+            Console.WriteLine("====================================");
+            Console.WriteLine("Digite o ID do Cliente que você quer remover");
+            Console.WriteLine("====================================\n");
+            int id = int.Parse(Console.ReadLine());
+
+            if (id > 0 && id < clientes.Count)
+            {
+                clientes.RemoveAt(id);
+                Salvar();
+            }
+            else
+            {
+                Console.WriteLine("ID Digitado é invalido");
+                Console.ReadLine();
+            }
+
+        }
+
     }
 }
